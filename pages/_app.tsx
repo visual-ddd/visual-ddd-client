84e1915f1938d5171d5169aed65d8b97b97278f0
黎贵奇 <carney520@hotmail.com>
2022-12-09 11:22:23 +0800

    chore: 新增 antd
    Change-Id: Id9ad4642dc1350805fdc3e6e9871fe6ae8651c38

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ConfigProvider } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import zhCN from 'antd/locale/zh_CN';
+import 'antd/dist/reset.css';
+
+import '../styles/globals.css';
+
+dayjs.locale('zh-cn');
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider locale={zhCN}>
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
