9f0442dd80949f15027c4645a5aa322e923d09ec
黎贵奇 <carney520@hotmail.com>
2023-01-03 15:14:33 +0800

    feat: 值对象验证
    Change-Id: I6d25881b052b1841acec8a75d8c580d94d3063a2

--- modules/domain/domain-design/shape/ValueObject/ValueObject.tsx
+++ modules/domain/domain-design/shape/ValueObject/ValueObject.tsx
@@ -1,7 +1,18 @@
-import { defineShape, ShapeComponentProps, useShapeModel } from '@/lib/editor';
+import { defineShape, FormRuleReportType, ROOT_FIELD, ShapeComponentProps, useShapeModel } from '@/lib/editor';
 import { ReactComponentBinding, ReactComponentProps, registerReactComponent } from '@/lib/g6-binding';
+import { getPrefixPath } from '@/lib/utils';
 
-import { ClassShape, createValueObject, DomainObjectName, ValueObjectDSL, ValueObjectEditor } from '../../dsl';
+import {
+  checkDomainObjectNameConflict,
+  checkPropertyName,
+  checkSameAggregationReference,
+  checkUnderAggregation,
+  ClassShape,
+  createValueObject,
+  DomainObjectName,
+  ValueObjectDSL,
+  ValueObjectEditor,
+} from '../../dsl';
 
 import icon from './value-object.png';
 
@@ -30,6 +41,94 @@ defineShape({
   description: '值对象',
   icon: icon,
   shapeType: 'node',
+
+  rules: {
+    fields: {
+      [ROOT_FIELD]: {
+        $self: [
+          {
+            // 检查是否在聚合下
+            async validator(value, context) {
+              checkUnderAggregation(context);
+            },
+            reportType: FormRuleReportType.Warning,
+          },
+          {
+            // 检查引用
+            async validator(value, context) {
+              checkSameAggregationReference(context);
+            },
+          },
+        ],
+      },
+      name: {
+        $self: [
+          { required: true, message: '标识符不能为空' },
+          {
+            async validator(value, context) {
+              // 检查命名是否冲突
+              checkDomainObjectNameConflict(value, context);
+            },
+          },
+        ],
+      },
+      title: { $self: [{ required: true, reportType: FormRuleReportType.Warning, message: '请填写标题' }] },
+      properties: {
+        $self: { type: 'array' },
+        '*': {
+          $self: { type: 'object' },
+          fields: {
+            name: {
+              $self: [
+                { required: true, message: '属性名不能为空' },
+                {
+                  async validator(value, context) {
+                    checkPropertyName(value, 'properties', context);
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+      methods: {
+        $self: { type: 'array' },
+        '*': {
+          $self: { type: 'object' },
+          fields: {
+            name: {
+              $self: [
+                { required: true, message: '方法名不能为空' },
+                {
+                  async validator(value, context) {
+                    checkPropertyName(value, 'methods', context);
+                  },
+                },
+              ],
+            },
+            parameters: {
+              $self: { type: 'array' },
+              '*': {
+                $self: { type: 'object' },
+                fields: {
+                  name: {
+                    $self: [
+                      { required: true, message: '参数名不能为空' },
+                      {
+                        async validator(value, context) {
+                          checkPropertyName(value, getPrefixPath(context.rawRule.fullField!, 'parameters'), context);
+                        },
+                      },
+                    ],
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 
   initialProps: () => {
     return { ...createValueObject(), zIndex: 2 };
