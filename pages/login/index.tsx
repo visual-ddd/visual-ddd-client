c2abad7f67b08b1ce684e5a7d325a0fc655caf2a
黎贵奇 <carney520@hotmail.com>
2023-02-08 15:14:07 +0800

    feat: 登录页面联调
    Change-Id: I6aa433735022b92af1e1e354d7823ece7099ead0

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,28 +1,73 @@
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, message } from 'antd';
+import { request } from '@/modules/backend-client';
 import { Logo } from '@/modules/user';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import s from './index.module.scss';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+interface LoginPayload {
+  accountNo: string;
+  password: string;
+}
 
 export default function Login() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const from = router.query.from as string | undefined;
 
   // 登录
-  const handleLogin = (values: any) => {
-    console.log('登录', values);
-    router.push('/launch');
+  const handleLogin = async (values: LoginPayload) => {
+    try {
+      setLoading(true);
+
+      await request.requestByPost('/api/login', values);
+
+      const url = new URL('/launch', globalThis.location.href);
+
+      if (from) {
+        url.searchParams.set('from', from);
+      }
+
+      // 登录成功
+      router.push(url);
+    } catch (err) {
+      message.error((err as Error).message);
+    } finally {
+      setLoading(false);
+    }
   };
 
+  useEffect(() => {
+    if (from) {
+      message.warning('会话失效，请重新登录');
+    }
+  }, [from]);
+
   return (
-    <Form name="loginForm" className={s.login} onFinish={handleLogin} layout="vertical" requiredMark={false}>
+    <Form<LoginPayload>
+      name="loginForm"
+      className={s.login}
+      onFinish={handleLogin}
+      layout="vertical"
+      requiredMark={false}
+    >
       <Form.Item>
         <Logo />
       </Form.Item>
 
-      <Form.Item name="username" label="账号" rules={[{ required: true, message: '请输入邮箱!' }]}>
+      <Form.Item
+        name="accountNo"
+        label="账号"
+        rules={[
+          { required: true, message: '请输入邮箱!' },
+          { type: 'email', message: '邮箱格式错误' },
+        ]}
+      >
         <Input placeholder="输入邮箱" />
       </Form.Item>
+
       <Form.Item name="password" label="密码" rules={[{ required: true, message: '请输入密码!' }]}>
         <Input type="password" placeholder="输入密码" />
       </Form.Item>
@@ -34,7 +79,7 @@ export default function Login() {
       </Form.Item>
 
       <Form.Item>
-        <Button type="primary" htmlType="submit" className={s.submit} block>
+        <Button type="primary" htmlType="submit" className={s.submit} block loading={loading}>
           登录
         </Button>
         <Link href="/login/register">
