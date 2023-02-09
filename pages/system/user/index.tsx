a27c0aeed12b00d6a291d46a90a6d71d60eefabc
林明 <linming@wakedata.com>
2023-02-09 11:08:11 +0800

    feat:引入antd pro 组件库
    Change-Id: Ib54865e1bd8a90cc34ba49dc07208bfc8463b369

--- pages/system/user/index.tsx
+++ pages/system/user/index.tsx
@@ -1,10 +1,17 @@
+import React from 'react';
+import { ProForm, ProFormText } from '@ant-design/pro-components';
 import { getLayout } from '@/modules/layout';
 
 export default function User() {
   return (
-    <div>
-      <div>我是用户管理</div>
-    </div>
+    <ProForm
+      onFinish={async values => {
+        console.log(values);
+      }}
+    >
+      <ProFormText name="name" label="姓名" />
+    </ProForm>
   );
 }
+
 User.getLayout = getLayout;
