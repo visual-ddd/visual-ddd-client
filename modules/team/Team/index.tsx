7abec03340b44c187c40c11f025912b1c42cd35a
黎贵奇 <carney520@hotmail.com>
2023-02-16 18:05:41 +0800

    feat: 团队主页
    Change-Id: Iaa3d6f419b917db94f694cda77bacd4a29658621

---
+++ modules/team/Team/index.tsx
@@ -0,0 +1,52 @@
+import { PreviewPageLayout, PreviewPageSection } from '@/lib/components/PreviewPageLayout';
+import { useLayoutTitle } from '@/modules/Layout';
+import { TeamDetail } from '@/modules/organization/types';
+import { Card, Statistic } from 'antd';
+import { useRouter } from 'next/router';
+import s from './index.module.scss';
+
+export interface TeamHomeProps {
+  detail: TeamDetail;
+}
+
+/**
+ * 团队首页
+ */
+export function TeamHome(props: TeamHomeProps) {
+  // @ts-expect-error
+  const router = useRouter();
+  const { detail } = props;
+  // @ts-expect-error
+  const teamId = detail.id;
+
+  useLayoutTitle(`团队 - ${detail.name}`);
+
+  return (
+    <PreviewPageLayout
+      className={s.root}
+      stats={
+        <>
+          <Card bordered size="small">
+            <Statistic value={10} title="业务域"></Statistic>
+          </Card>
+          <Card bordered size="small">
+            <Statistic value={10} title="业务场景"></Statistic>
+          </Card>
+          <Card bordered size="small">
+            <Statistic value={10} title="应用"></Statistic>
+          </Card>
+        </>
+      }
+      left={
+        <>
+          <Card size="small" title="简介">
+            <PreviewPageSection name="描述">{detail.description || '未配置描述'}</PreviewPageSection>
+            <PreviewPageSection name="所属组织">{detail.organizationDTO?.name}</PreviewPageSection>
+          </Card>
+        </>
+      }
+    ></PreviewPageLayout>
+  );
+}
+
+export default TeamHome;
--- pages/team/[id]/index.tsx
+++ pages/team/[id]/index.tsx
@@ -1,11 +1,33 @@
+import { withWakedataRequestSsr } from '@/modules/session/ssr-helper';
+import dynamic from 'next/dynamic';
+import type { TeamHomeProps } from '@/modules/team/Team';
+import type { TeamDetail } from '@/modules/organization/types';
 import { getTeamLayout } from '@/modules/team/TeamLayout';
+
+const TeamHome = dynamic(() => import('@/modules/team/Team'), { ssr: false });
 
 /**
  * 团队首页
  * @returns
  */
-export default function Team() {
-  return <div>TODO: 团队首页</div>;
+export default function Team(props: TeamHomeProps) {
+  return <TeamHome {...props} />;
 }
 
 Team.getLayout = getTeamLayout;
+
+export const getServerSideProps = withWakedataRequestSsr<TeamHomeProps>(async context => {
+  const { id } = context.params as { id: string };
+
+  const detail = await context.req.request<TeamDetail>(
+    '/wd/visual/web/team/team-detail-query',
+    {
+      id,
+    },
+    { method: 'GET' }
+  );
+
+  return {
+    props: { detail },
+  };
+});
