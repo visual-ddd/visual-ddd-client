210853effeebf9af68e8f706085c743d60bb05a1
黎贵奇 <carney520@hotmail.com>
2023-02-27 12:15:40 +0800

    feat: 重构 ScenarioDesignerModel
    Change-Id: Ia073f59dd2a85d181c703694a43c12fcac8698d1

--- lib/components/Completion/Completion.tsx
+++ lib/components/Completion/Completion.tsx
@@ -1,14 +1,15 @@
 import { NameCase } from '@/lib/core';
+import { NoopArray } from '@wakeapp/utils';
 import { useCompletionContext } from './CompletionContext';
 
 export function useIdentifierCompletion(wordCase: NameCase) {
   const context = useCompletionContext();
 
-  return (value: string) => context!.getCompletion(wordCase).search(value);
+  return (value: string) => context?.getCompletion(wordCase).search(value) ?? NoopArray;
 }
 
 export function useCompletion() {
   const context = useCompletionContext();
 
-  return (value: string) => context!.getCompletion('any').search(value);
+  return (value: string) => context?.getCompletion('any').search(value) ?? NoopArray;
 }
