8b181a2ace12a917c484ffff8470d0ee9a499227
黎贵奇 <carney520@hotmail.com>
2023-04-06 11:08:32 +0800

    feat: 首页展示 导航菜单
    Change-Id: I8d0208c63aecb4f420705af896d3b2bf313c5984

--- modules/home/index.tsx
+++ modules/home/index.tsx
@@ -3,17 +3,28 @@ import { useRouter } from 'next/router';
 
 import s from './index.module.scss';
 import cover from './pic.png';
+import Link from 'next/link';
 
 export const Home = () => {
   const router = useRouter();
 
   return (
     <div className={s.root}>
-      <div className={s.body}>
+      <header className={s.header}>
         <div className={s.logo}>
           <img src="/logo.svg" alt="logo" />
           <span>Visual DDD</span>
         </div>
+        <div className={s.menus}>
+          <Link href="/chat" className={s.menu}>
+            Chat
+          </Link>
+          <Link href="/dashboard" className={s.menu}>
+            登录
+          </Link>
+        </div>
+      </header>
+      <div className={s.body}>
         <main className={s.main}>
           <h1>可视化的 DDD 开发平台</h1>
           <h2>AI + 可视化 + DDD + 代码生成， 赋能企业业务开发</h2>
