6813ef1b161296075ba1d853aee20ae0d8b0972f
黎贵奇 <carney520@hotmail.com>
2023-04-11 16:11:44 +0800

    fix: 修复百度脚本没有加载
    Change-Id: Ib3a2dab81515481803308c1fd07a117d5136f983

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,33 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+/**
+ * 百度 统计
+ * @returns
+ */
+const BaiduAnalyze = () => {
+  if (!process.env.BD_ANALYZE_KEY) {
+    return null;
+  }
+
+  return (
+    <script
+      id="baidu-analyze"
+      defer
+      dangerouslySetInnerHTML={{
+        __html: `
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?${process.env.BD_ANALYZE_KEY}";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+  `,
+      }}
+    ></script>
+  );
+};
+
 export default function Document() {
   return (
     <Html>
@@ -9,6 +37,7 @@ export default function Document() {
           noModule
           src="https://www.wakedt.cn/__polyfill__?features=es2022%2Ces2021%2Ces2020%2Ces2019%2Ces2018%2Ces2017%2Ces2016%2Ces2015%2Cdefault%2CglobalThis"
         ></script>
+        <BaiduAnalyze />
       </Head>
       <body>
         <Main />
