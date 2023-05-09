04355ed593ac036bb136b01bdb45f3f514625049
黎贵奇 <carney520@hotmail.com>
2023-05-09 13:42:46 +0800

    feat: 支持 processon
    Change-Id: I750f1ab69742026e133cbd2ac36ceadfc5f79b9f

---
+++ lib/wysiwyg-editor/CustomBlock/ProcessOn.tsx
@@ -0,0 +1,36 @@
+import { registerBlock } from '../ReactBlock';
+import { ThirdPartyBlock, ThirdPartyBlockState } from './ThirdPartyBlock';
+import { ProcessOnIcon } from './ProcessOnIcon';
+
+registerBlock<ThirdPartyBlockState>({
+  name: 'processon',
+  title: 'ProcessOn',
+  icon: ProcessOnIcon,
+  initialState: () => ({ url: '', height: 500 }),
+  render(state, updateState) {
+    const handleChange = (value: string) => {
+      try {
+        const url = new URL(value);
+
+        if (!url.host.includes('processon.com') || !url.pathname.startsWith('/embed/')) {
+          throw TypeError('地址错误');
+        }
+
+        updateState({ url: value });
+      } catch (error) {
+        throw new Error('请输入正确的 ProcessOn 地址, 例如 https://www.processon.com/embed/64599fcec9b6e303a6d2bccb ');
+      }
+    };
+
+    return (
+      <ThirdPartyBlock
+        placeholder="输入 ProcessOn 链接(分享协作 > 嵌入第三方 > 复制链接)"
+        exampleValue="https://www.processon.com/embed/64599fcec9b6e303a6d2bccb"
+        value={state.url}
+        height={state.height}
+        onHeightChange={height => updateState({ height })}
+        onChange={handleChange}
+      ></ThirdPartyBlock>
+    );
+  },
+});
--- lib/wysiwyg-editor/CustomBlock/index.ts
+++ lib/wysiwyg-editor/CustomBlock/index.ts
@@ -1,2 +1,3 @@
 // export * from './Demo'
 export * from './CodeSandbox';
+export * from './ProcessOn';
